1cac0605140e79b20741ca7f97c33be11439a037
Far-Traveller <carriere.aurore@orange.fr>
2022-07-04 23:06:12 +0200

    deleted test to modify password

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,31 +2,26 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use App\Form\UserFormType;
-use App\Form\UserPasswordFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/profil/{id}', name: 'app_user')]
-    public function index(User $user,
-                          Request $request,
-                          EntityManagerInterface $entityManager,
-                          UserPasswordHasherInterface $userPasswordHasher): Response
+    #[Route('/profil', name: 'app_user')]
+    public function index(Request $request,
+                          EntityManagerInterface $entityManager): Response
     {
         //Check if user is connected and if he's trying to go on his profile
-        if (!$this->getUser() || $this->getUser() !== $user) {
+        if (!$this->getUser()) {
             return $this->redirectToRoute('app_homepage');
         }
 
         //Form to modify infos of the current user
-        $form = $this->createForm(UserFormType::class, $user);
+        $form = $this->createForm(UserFormType::class, $this->getUser());
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $user = $form->getData();
@@ -38,42 +33,7 @@ class UserController extends AbstractController
                 'Vos informations ont bien été modifiées !!'
             );
 
-            return $this->redirectToRoute('app_user', array('id' => $user->getId()));
-        }
-
-        //Form to modify password of the current user
-        $passwordForm = $this->createForm(UserPasswordFormType::class, $user);
-        $passwordForm->handleRequest($request);
-        if ($passwordForm->isSubmitted() && $passwordForm->isValid()) {
-            if($userPasswordHasher->isPasswordValid($user, $passwordForm->get('newPassword')->getData())) {
-
-                // encode the plain password
-                $user->setPassword(
-                    $userPasswordHasher->hashPassword(
-                        $user,
-                        $passwordForm->get('newPassword')->getData()
-                    )
-                );
-
-                $entityManager->persist($user);
-                $entityManager->flush();
-
-                $this->addFlash(
-                    'success',
-                    'Votre mot de passe a bien été modifié !!'
-                );
-
-                return $this->redirectToRoute('app_user', array('id' => $user->getId()));
-            } else {
-                echo($passwordForm->get('plainPassword')->getData());
-                echo($passwordForm->get('newPassword')->getData());
-
-                $this->addFlash(
-                    'warning',
-                    'Le mot de passe est incorrect'
-                );
-            }
-
+            return $this->redirectToRoute('app_user');
         }
 
         $favorite_stories = $this->getUser()->getStories();
@@ -82,7 +42,6 @@ class UserController extends AbstractController
             'controller_name' => 'UserController',
             'favorite_stories' => $favorite_stories,
             'editInfosForm' => $form->createView(),
-            'editPasswordForm' => $passwordForm->createView(),
         ]);
     }
 }
